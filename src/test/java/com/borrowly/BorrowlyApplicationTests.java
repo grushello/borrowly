4d14e2a7180d089275a4d71ac76365ae9628b2a9
grushello <grushello@gmail.com>
2026-07-10 11:05:28 +0300

    initial commit

---
+++ src/test/java/com/borrowly/BorrowlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borrowly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
