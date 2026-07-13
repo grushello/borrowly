91b5d5828a2ee1abec9fbe53817468bf2dea1cad
Francesco Doria <doria5@live.it>
2026-07-13 17:42:24 +0300

    item image model

--- src/main/java/com/borrowly/model/item/ItemImage.java
+++ src/main/java/com/borrowly/model/item/ItemImage.java
@@ -1,4 +1,46 @@
 package com.borrowly.model.item;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+@Getter
+@Table(name = "item_images")
+@Entity
+@ToString(exclude = {"imageData"})
 public class ItemImage {
+    @Id
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+
+    @NotNull
+    @Column(columnDefinition = "BYTEA", nullable = false)
+    private byte[] imageData;
+
+    @NotBlank
+    @Size(max = 255)
+    private String fileName;
+
+    @NotBlank
+    @Size(max = 100)
+    private String contentType;
+
+    @NotNull
+    @Builder.Default
+    private Boolean primaryImage = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Item item;
 }
