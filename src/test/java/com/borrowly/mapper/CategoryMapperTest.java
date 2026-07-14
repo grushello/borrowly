b6d825326dda1867a0dc85227bb417184d8a2eb4
Francesco Doria <doria5@live.it>
2026-07-14 17:40:53 +0300

    start writing category mapper tests

--- src/test/java/com/borrowly/mapper/CategoryMapperTest.java
+++ src/test/java/com/borrowly/mapper/CategoryMapperTest.java
@@ -1,4 +1,35 @@
 package com.borrowly.mapper;
 
-public class CategoryMapperTest {
+import com.borrowly.dto.request.CategoryRequest;
+import com.borrowly.dto.response.CategoryResponse;
+import com.borrowly.model.item.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryMapperTest {
+    CategoryMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new CategoryMapperImpl();
+    }
+
+    @Test
+    void toResponse_MapsCategoryFields() {
+        Category category = Category.builder().name("fishing").description("All things fishing.").build();
+        CategoryResponse response = mapper.toResponse(category);
+
+        assertEquals(category.getName(), response.name());
+        assertEquals(category.getDescription(), response.description());
+    }
+
+    @Test
+    void toResponse_RequestToEntity() {
+        Category category = Category.builder().name("fishing").description("All things fishing.").build();
+        CategoryRequest request = new CategoryRequest(category.getName(),  category.getDescription());
+
+        // TODO
+    }
 }
