cb10d34c17ced33522769f6c416d0fd8f7063d95
Mykhailo Osadchuk <m.osadchuk2201@gmail.com>
2026-07-14 14:58:19 +0300

    feature: Review model class and unit tests (#49)

--- src/main/java/com/borrowly/model/user/Review.java
+++ src/main/java/com/borrowly/model/user/Review.java
@@ -1,4 +1,49 @@
 package com.borrowly.model.user;
 
+import com.borrowly.model.rental.Rental;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reviews")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(exclude = {"reviewer", "rental"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Review {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Size(max = 2000)
+    @Column(length = 2000)
+    private String comment;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "rental_id", nullable = false)
+    private Rental rental;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reviewer_id", nullable = false)
+    private User reviewer;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
