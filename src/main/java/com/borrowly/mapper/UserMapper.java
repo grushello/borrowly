db61f45a107301f1cf132c46046163d31e71d7ba
Francesco Doria <doria5@live.it>
2026-07-14 16:35:51 +0300

    user mapper interface

--- src/main/java/com/borrowly/mapper/UserMapper.java
+++ src/main/java/com/borrowly/mapper/UserMapper.java
@@ -1,7 +1,17 @@
 package com.borrowly.mapper;
 
-import org.mapstruct.Mapper;
+import com.borrowly.dto.request.UpdateUserRequest;
+import com.borrowly.dto.response.UserResponse;
+import com.borrowly.dto.response.UserSummaryResponse;
+import com.borrowly.model.user.User;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
-public class UserMapper {
+public interface UserMapper {
+    UserResponse toResponse(User user);
+
+    UserSummaryResponse toSummary(User user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(@MappingTarget User target, UpdateUserRequest source);
 }
