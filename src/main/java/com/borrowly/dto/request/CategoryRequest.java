984413a288aa854a0f735ccc8752151ccb390f33
Francesco Doria <doria5@live.it>
2026-07-14 17:13:11 +0300

    category request dto

---
+++ src/main/java/com/borrowly/dto/request/CategoryRequest.java
@@ -0,0 +1,11 @@
+package com.borrowly.dto.request;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoryRequest(
+        @NotBlank @Size(max = 100, message = "Name cannot have more than 100 characters.") String name,
+        @Size(max = 500, message = "Description cannot have more than 100 characters.") @Nullable String description
+) {
+}
