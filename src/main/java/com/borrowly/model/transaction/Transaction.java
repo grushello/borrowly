9e19ded7679e500fd10961b414e54a67cec515df
grushello <grushello@gmail.com>
2026-07-14 11:56:36 +0300

    fix: remove conflicting @GeneratedValue annotation

--- src/main/java/com/borrowly/model/transaction/Transaction.java
+++ src/main/java/com/borrowly/model/transaction/Transaction.java
@@ -25,7 +25,6 @@ import java.util.UUID;
 public class Transaction {
 
     @Id
-    @GeneratedValue
     @Column(nullable = false, updatable = false)
     @Builder.Default
     private UUID id = UUID.randomUUID();
