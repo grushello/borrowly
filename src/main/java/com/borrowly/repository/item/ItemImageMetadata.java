c18360bbf2941d169342a5f0dc4f2013389574a9
grusha <124006415+grushello@users.noreply.github.com>
2026-07-15 10:33:03 +0300

    feature: add ItemImageRepository and ItemImageMetadata with corresponding tests (#62)
    * feature: add ItemImageRepository and ItemImageMetadata with corresponding tests
    
    * fix: fix naming

---
+++ src/main/java/com/borrowly/repository/item/ItemImageMetadata.java
@@ -0,0 +1,17 @@
+package com.borrowly.repository.item;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface ItemImageMetadata {
+
+    UUID getId();
+
+    String getFileName();
+
+    String getContentType();
+
+    Boolean getPrimaryImage();
+
+    LocalDateTime getCreatedAt();
+}
