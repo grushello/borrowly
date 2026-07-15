91b5d9fc9f7661a3bfe9c907ef2255a4bbdc5176
grusha <124006415+grushello@users.noreply.github.com>
2026-07-15 17:06:12 +0300

    feature: add itemRepository with tests (#60)

--- src/main/java/com/borrowly/repository/item/ItemRepository.java
+++ src/main/java/com/borrowly/repository/item/ItemRepository.java
@@ -1,4 +1,36 @@
 package com.borrowly.repository.item;
 
-public class ItemRepository {
+import com.borrowly.model.item.Item;
+import com.borrowly.model.item.ItemStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ItemRepository extends JpaRepository<Item, UUID>, JpaSpecificationExecutor<Item> {
+
+    @EntityGraph(attributePaths = {"owner", "category"})
+    Page<Item> findByOwner_IdAndStatusNot(
+            UUID ownerId,
+            ItemStatus excluded,
+            Pageable pageable
+    );
+
+
+    @EntityGraph(attributePaths = {"owner", "category"})
+    Page<Item> findByStatus(
+            ItemStatus status,
+            Pageable pageable
+    );
+
+
+    @EntityGraph(attributePaths = {"owner", "category"})
+    Optional<Item> findByIdAndStatusNot(
+            UUID id,
+            ItemStatus excluded
+    );
 }
