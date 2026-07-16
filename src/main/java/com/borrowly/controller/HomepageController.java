dacbdc1d972ad469efb69fb8e20b21d9118442e2
Francesco Doria <doria5@live.it>
2026-07-16 17:00:08 +0300

    v2

--- src/main/java/com/borrowly/controller/HomepageController.java
+++ src/main/java/com/borrowly/controller/HomepageController.java
@@ -1,15 +1,24 @@
 package com.borrowly.controller;
 
+import com.borrowly.dto.response.ItemResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class HomepageController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(@PageableDefault(size = 12, sort = "title") Pageable pageable,
+                        Model model){
+//        Page<ItemResponse> itemPage = itemService.findAll(pageable);
+//        model.addAttribute("itemPage", itemPage);
         return "index";
     }
 
@@ -18,8 +27,23 @@ public class HomepageController {
         return "signup";
     }
 
+    @GetMapping("/signin")
+    public String signin(){
+        return "signin";
+    }
+
     @GetMapping("/dashboard")
     public String dashboard(){
         return "dashboard";
+    }
+
+    @GetMapping("/item/{id}")
+    public String getItemDetail(@PathVariable("id") String id, Model model) {
+
+        // ItemResponse item = itemService.findById(id);
+
+
+        model.addAttribute("fakeItemId", id);
+        return "item";
     }
 }
