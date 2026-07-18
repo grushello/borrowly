26ef15b284c6b8239ddbc8683b80946fe38643ad
Francesco Doria <doria5@live.it>
2026-07-18 14:10:34 +0300

    remove unused imports

--- src/main/java/com/borrowly/service/CategoryService.java
+++ src/main/java/com/borrowly/service/CategoryService.java
@@ -9,11 +9,8 @@ import com.borrowly.mapper.CategoryMapper;
 import com.borrowly.model.item.Category;
 import com.borrowly.repository.item.CategoryRepository;
 import com.borrowly.repository.item.ItemRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
