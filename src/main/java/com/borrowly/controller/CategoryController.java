c357194a4564cd96ed176bfa30117aea075f19d8
Francesco Doria <doria5@live.it>
2026-07-18 12:57:47 +0300

    @Valid annotation to return validation details in response

--- src/main/java/com/borrowly/controller/CategoryController.java
+++ src/main/java/com/borrowly/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.borrowly.dto.request.CategoryRequest;
 import com.borrowly.dto.response.CategoryResponse;
 import com.borrowly.service.CategoryService;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,13 +37,13 @@ public class CategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/api/admin/categories")
     @Transactional
-    public CategoryResponse addCategory(@RequestBody CategoryRequest categoryRequest) {
+    public CategoryResponse addCategory(@Valid @RequestBody CategoryRequest categoryRequest) {
         return categoryService.add(categoryRequest);
     }
 
     @Transactional
     @PatchMapping("/api/admin/categories/{id}")
-    public CategoryResponse updateCategory(@PathVariable UUID id, @RequestBody CategoryRequest categoryRequest) {
+    public CategoryResponse updateCategory(@PathVariable UUID id, @Valid @RequestBody CategoryRequest categoryRequest) {
         return categoryService.update(id, categoryRequest);
     }
 
