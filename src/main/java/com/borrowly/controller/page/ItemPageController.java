819d21a9da43e935a5342f74207009973c3784eb
Francesco Doria <doria5@live.it>
2026-07-18 09:18:05 +0300

    item page controller: get data about item

--- src/main/java/com/borrowly/controller/page/ItemPageController.java
+++ src/main/java/com/borrowly/controller/page/ItemPageController.java
@@ -1,17 +1,25 @@
 package com.borrowly.controller.page;
 
+import com.borrowly.dto.response.ItemResponse;
+import com.borrowly.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemPageController {
+    private final ItemService itemService;
+
     @GetMapping("/item/{id}")
-    public String getItemDetail(@PathVariable("id") String id, Model model) {
-        model.addAttribute("fakeItemId", id);
+    public String getItemDetail(@PathVariable("id") UUID id, Model model) {
+        ItemResponse item = itemService.getById(id);
+
+        model.addAttribute("item", item);
         return "item";
     }
 }
