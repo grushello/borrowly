93567d6eda3ef983639ddfa4088539e7b5483469
Francesco Doria <doria5@live.it>
2026-07-19 09:57:53 +0300

    isFavorite logic in item page controller

--- src/main/java/com/borrowly/controller/page/ItemPageController.java
+++ src/main/java/com/borrowly/controller/page/ItemPageController.java
@@ -1,6 +1,7 @@
 package com.borrowly.controller.page;
 
 import com.borrowly.dto.response.ItemResponse;
+import com.borrowly.service.favorite.FavoriteService;
 import com.borrowly.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemPageController {
     private final ItemService itemService;
+    private final FavoriteService favoriteService;
 
     @GetMapping("/item/{id}")
-    public String getItemDetail(@PathVariable("id") UUID id, Model model) {
+    public String getItemDetail(@PathVariable("id") UUID id, Model model, Principal principal) {
         ItemResponse item = itemService.getById(id);
 
+        boolean isFavorite = false;
+        if (principal != null) {
+            isFavorite = favoriteService.isFavoritedByCurrentUser(id);
+        }
+        
+        model.addAttribute("isFavorite", isFavorite);
         model.addAttribute("item", item);
         return "item";
     }
