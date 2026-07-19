5534205979ec229b4237c3efe6fd26e12f3d4a94
Francesco Doria <doria5@live.it>
2026-07-19 14:06:29 +0300

    isOwner variable in item page controller

--- src/main/java/com/borrowly/controller/page/ItemPageController.java
+++ src/main/java/com/borrowly/controller/page/ItemPageController.java
@@ -1,9 +1,13 @@
 package com.borrowly.controller.page;
 
 import com.borrowly.dto.response.ItemResponse;
+import com.borrowly.security.CurrentUserProvider;
 import com.borrowly.service.favorite.FavoriteService;
 import com.borrowly.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,18 +21,24 @@ import java.util.UUID;
 public class ItemPageController {
     private final ItemService itemService;
     private final FavoriteService favoriteService;
+    private final CurrentUserProvider currentUserProvider;
 
     @GetMapping("/item/{id}")
     public String getItemDetail(@PathVariable("id") UUID id, Model model, Principal principal) {
         ItemResponse item = itemService.getById(id);
 
         boolean isFavorite = false;
+        boolean isOwner = false;
         if (principal != null) {
             isFavorite = favoriteService.isFavoritedByCurrentUser(id);
+            UUID currentUserId = currentUserProvider.getCurrentUser().getId();
+            assert currentUserId != null;
+            isOwner = currentUserId.equals(item.owner().id());
         }
 
         model.addAttribute("isFavorite", isFavorite);
         model.addAttribute("item", item);
+        model.addAttribute("isOwner", isOwner);
         return "item";
     }
 }
