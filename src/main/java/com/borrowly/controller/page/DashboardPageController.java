33b20250bd19ff3ff69d6238c5e72e747763b541
Francesco Doria <doria5@live.it>
2026-07-19 10:25:47 +0300

    get current user favorites in dashboard page controller

--- src/main/java/com/borrowly/controller/page/DashboardPageController.java
+++ src/main/java/com/borrowly/controller/page/DashboardPageController.java
@@ -1,14 +1,29 @@
 package com.borrowly.controller.page;
 
+import com.borrowly.dto.response.FavoriteResponse;
+import com.borrowly.service.favorite.FavoriteService;
+import com.borrowly.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class DashboardPageController {
+    private final FavoriteService favoriteService;
+
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(Model model){
+        Pageable widgetPageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        Page<FavoriteResponse> favoritesPage = favoriteService.listForCurrentUser(widgetPageable);
+
+        model.addAttribute("favorites", favoritesPage.getContent());
         return "dashboard";
     }
 }
