8e9fa320775a5f2ffbeaa291ec216fc1708dedb7
Francesco Doria <doria5@live.it>
2026-07-19 09:02:54 +0300

    get categories from catalog page controller

--- src/main/java/com/borrowly/controller/page/CatalogPageController.java
+++ src/main/java/com/borrowly/controller/page/CatalogPageController.java
@@ -1,7 +1,9 @@
 package com.borrowly.controller.page;
 
+import com.borrowly.dto.response.CategoryResponse;
 import com.borrowly.dto.response.ItemSummaryResponse;
 import com.borrowly.model.item.ItemCondition;
+import com.borrowly.service.CategoryService;
 import com.borrowly.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class CatalogPageController {
     private final ItemService itemService;
+    private final CategoryService categoryService;
 
     @GetMapping("/")
     public String index(@RequestParam(required = false) UUID categoryId,
@@ -30,6 +34,7 @@ public class CatalogPageController {
                         Model model){
 
         Page<ItemSummaryResponse> itemPage = itemService.browseItems(categoryId, condition, minPrice, maxPrice, search, pageable);
+        List<CategoryResponse> categories = categoryService.findAll();
 
         model.addAttribute("itemPage", itemPage);
         model.addAttribute("currentSearch", search);
@@ -37,6 +42,7 @@ public class CatalogPageController {
         model.addAttribute("currentMaxPrice", maxPrice);
         model.addAttribute("itemCondition", condition);
         model.addAttribute("categoryId", categoryId);
+        model.addAttribute("categories", categories);
 
         return "index";
     }
