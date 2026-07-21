562890305cd857248ec1445c073d6fccd6337ea1
Karyna <yatsenkokaryna2703@gmail.com>
2026-07-21 14:03:29 +0300

    refactor(User): exception review

--- src/main/java/com/borrowly/exception/UserNotFoundException.java
+++ src/main/java/com/borrowly/exception/UserNotFoundException.java
@@ -1,11 +1,7 @@
 package com.borrowly.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.UUID;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(UUID id) {
