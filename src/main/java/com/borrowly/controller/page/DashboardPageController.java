6e22f0f530706ca0787b4bf2bfe4cc57bac729dc
Francesco Doria <doria5@live.it>
2026-07-21 21:13:49 +0300

    active rentals as owner and borrower to the dashboard page

--- src/main/java/com/borrowly/controller/page/DashboardPageController.java
+++ src/main/java/com/borrowly/controller/page/DashboardPageController.java
@@ -2,12 +2,16 @@ package com.borrowly.controller.page;
 
 import com.borrowly.dto.response.FavoriteResponse;
 import com.borrowly.dto.response.ItemSummaryResponse;
+import com.borrowly.dto.response.RentalResponse;
 import com.borrowly.dto.response.UserResponse;
 import com.borrowly.mapper.UserMapper;
+import com.borrowly.model.rental.RentalStatus;
 import com.borrowly.model.user.User;
 import com.borrowly.security.CurrentUserProvider;
 import com.borrowly.service.favorite.FavoriteService;
+import com.borrowly.service.item.ItemImageService;
 import com.borrowly.service.item.ItemService;
+import com.borrowly.service.rental.RentalService;
 import com.borrowly.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,12 +22,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class DashboardPageController {
     private final FavoriteService favoriteService;
     private final ItemService itemService;
     private final UserService userService;
+    private final RentalService rentalService;
 
     @GetMapping("/dashboard")
     public String dashboard(Model model){
@@ -33,9 +40,16 @@ public class DashboardPageController {
         Page<ItemSummaryResponse> itemsPage = itemService.getCurrentUserItems(widgetPageable);
         UserResponse userResponse = userService.getProfile();
 
+        Pageable activeRentalPageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "endDate"));
+
+        Page<RentalResponse> activeRentalsAsBorrower = rentalService.listAsBorrower(List.of(RentalStatus.ACTIVE), activeRentalPageable);
+        Page<RentalResponse> activeRentalsAsOwner = rentalService.listAsOwner(List.of(RentalStatus.ACTIVE), activeRentalPageable);
+
         model.addAttribute("favorites", favoritesPage.getContent());
         model.addAttribute("userItemListings", itemsPage.getContent());
         model.addAttribute("user", userResponse);
+        model.addAttribute("activeRentalsAsBorrower", activeRentalsAsBorrower);
+        model.addAttribute("activeRentalsAsOwner", activeRentalsAsOwner);
         return "dashboard";
     }
 }
---
+++ src/main/java/com/borrowly/controller/page/RentalRequestsPageController.java
@@ -0,0 +1,32 @@
+package com.borrowly.controller.page;
+
+import com.borrowly.dto.response.RentalRequestResponse;
+import com.borrowly.model.rental.RentalRequestStatus;
+import com.borrowly.service.rental.RentalService;
+import com.borrowly.service.rentalrequest.RentalRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class RequestsPageController {
+
+    private final RentalService rentalService;
+    private final RentalRequestService rentalRequestService;
+
+    @GetMapping("/rental-requests")
+    public String getRequests(Model model) {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "startDate"));
+
+        Page<RentalRequestResponse> pendingRentalRequests = rentalRequestService.getIncoming(RentalRequestStatus.PENDING, pageable);
+        model.addAttribute("pending", pendingRentalRequests);
+        return "rental-requests";
+    }
+
+}
