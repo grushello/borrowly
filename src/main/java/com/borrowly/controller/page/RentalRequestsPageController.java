3dc0149f53524266d03dd003c64e8a8eb3bfff1c
Francesco Doria <doria5@live.it>
2026-07-22 13:47:51 +0300

    pagination of rental requests page

--- src/main/java/com/borrowly/controller/page/RentalRequestsPageController.java
+++ src/main/java/com/borrowly/controller/page/RentalRequestsPageController.java
@@ -12,20 +12,21 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class RentalRequestsPageController {
-    
+
     private final RentalRequestService rentalRequestService;
 
     @GetMapping("/rental-requests")
-    public String getRequests(Model model) {
-        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "startDate"));
+    public String getRequests(@RequestParam(defaultValue = "0") int incomingPage, @RequestParam(defaultValue = "0") int outgoingPage, Model model) {
+        Pageable pageableIncoming = PageRequest.of(incomingPage, 4, Sort.by(Sort.Direction.DESC, "startDate"));
+        Pageable pageableOutgoing = PageRequest.of(outgoingPage, 4, Sort.by(Sort.Direction.DESC, "startDate"));
 
-
-        Page<RentalRequestResponse> pendingIncomingRentalRequests = rentalRequestService.getIncoming(RentalRequestStatus.PENDING, pageable);
-        Page<RentalRequestResponse> pendingOutgoingRentalRequests = rentalRequestService.getOutgoing(RentalRequestStatus.PENDING, pageable);
+        Page<RentalRequestResponse> pendingIncomingRentalRequests = rentalRequestService.getIncoming(RentalRequestStatus.PENDING, pageableIncoming);
+        Page<RentalRequestResponse> pendingOutgoingRentalRequests = rentalRequestService.getOutgoing(RentalRequestStatus.PENDING, pageableOutgoing);
 
         model.addAttribute("pendingIncoming", pendingIncomingRentalRequests);
         model.addAttribute("pendingOutgoing", pendingOutgoingRentalRequests);
