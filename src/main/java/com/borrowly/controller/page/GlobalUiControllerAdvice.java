4967974de5a1022ffc86e7a38c1f69c11d8028fc
Francesco Doria <doria5@live.it>
2026-07-22 19:44:56 +0300

    change GlobalUiControllerAdvice because it was interfering with tests related to notifications

--- src/main/java/com/borrowly/controller/page/GlobalUiControllerAdvice.java
+++ src/main/java/com/borrowly/controller/page/GlobalUiControllerAdvice.java
@@ -2,6 +2,7 @@ package com.borrowly.controller.page;
 
 import com.borrowly.dto.response.NotificationResponse;
 import com.borrowly.service.notification.NotificationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,20 +14,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.security.Principal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @ControllerAdvice(annotations = Controller.class)
 @RequiredArgsConstructor
 public class GlobalUiControllerAdvice {
 
-    private final NotificationService notificationService;
+    private final Optional<NotificationService> notificationService;
+
     @ModelAttribute("notifications")
-    public List<NotificationResponse> globalNotifications(Principal principal) {
-        if (principal == null) {
+    public List<NotificationResponse> globalNotifications(Principal principal, HttpServletRequest request) {
+
+        if (request.getRequestURI().startsWith("/api/")) {
+            return Collections.emptyList();
+        }
+
+        if (principal == null || notificationService.isEmpty()) {
             return Collections.emptyList();
         }
 
         Pageable topFive = PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "createdAt"));
-
-        return notificationService.listForCurrentUser(topFive).getContent();
+        return notificationService.get().listForCurrentUser(topFive).getContent();
     }
 }
